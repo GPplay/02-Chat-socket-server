d83621141d97cc68ccdab20dd4c3afa97fbedb52
GPplay <gybramdali2002@hotmail.com>
2025-01-11 18:19:05 -0500

    verificar que monda pasa con mongo

--- database/config.js
+++ database/config.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try{
         
-        await mongoose.connect(process.env.DB_CNN, {
-
-        });
+        await mongoose.connect(process.env.DB_CNN);
         console.log('Base de datos online');
 
     }catch(error){
