4f4766136f897809370e225b8482965a8fa37fff
GPplay <gybramdali2002@hotmail.com>
2025-01-11 18:12:12 -0500

    no se conecta a mongo por algun motivo que me gustaria saber cual es

--- database/config.js
+++ database/config.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try{
         
-        await mongoose.connect(process.env.DB_CNN, {});
+        await mongoose.connect(process.env.DB_CNN, {
+
+        });
         console.log('Base de datos online');
 
     }catch(error){
