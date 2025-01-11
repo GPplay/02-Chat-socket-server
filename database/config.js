93f48799f0b2d95432bfd350e9b245e3b56183f3
GPplay <gybramdali2002@hotmail.com>
2025-01-11 18:25:46 -0500

    revisar las entradas de mongo atlas

--- database/config.js
+++ database/config.js
@@ -5,7 +5,6 @@ const dbConnection = async() => {
         
         await mongoose.connect(process.env.DB_CNN);
         console.log('Base de datos online');
-
     }catch(error){
         console.log(error);
         throw new Error('Error a la hora de iniciar la base de datos - hable con gybram');
