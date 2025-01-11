1ed6055efb413e20996979556ce3c9b1afc548f3
GPplay <gybramdali2002@hotmail.com>
2025-01-11 17:51:54 -0500

    error

--- database/config.js
+++ database/config.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try{
-        mongoose.connect(process.env.DB_CNN, {
-            
+        
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
+
+        console.log('Base de datos online');
+
     }catch(error){
         console.log(error);
         throw new Error('Error a la hora de iniciar la base de datos - hable con gybram');
