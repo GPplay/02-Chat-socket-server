6faf8b2f673e2dc62d8a486b5fe23fa23ccf1964
GPplay <gybramdali2002@hotmail.com>
2025-02-16 18:34:48 -0500

    autenticanciones

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,19 @@
 const { response } = require("express")
+const { validationResult } = require("express-validator")
 
 
 const crearUsuario = async (req, res = response) => {
+
+    const errores = validationResult(req);
+
+    if (!errores.isEmpty()) {
+
+        return res.status(400).json({
+            ok: false,
+            errores: errores.mapped()
+        });
+    }
+
     res.json({
         ok: true,
         msg: 'Crear usuario!!!'
--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,19 @@
 /* 
-    path = 7api/login
+    path = /api/login
 */
 
 const {Router, response}=  require('express');
 const { crearUsuario } = require('../controllers/authController');
+const { check } = require('express-validator');
 
 const router = Router();
 
 
-router.post('/new', crearUsuario);
-
-
-
-
-
+router.post('/new', [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+],crearUsuario);
 
 
 
